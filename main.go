a53e15ee9306f2e0a7c5d91ff865144796fcda9b
José Sousa <jose.sousa@anchorlabs.com>
2025-02-26 10:13:54 +0000

    clear all networks in a single run

--- main.go
+++ main.go
@@ -11,36 +11,81 @@ type Vnet struct {
 	Id string `json:"id"`
 }
 
+const TenderlyOptimismURL = "https://api.tenderly.co/api/v1/account/anchorage/project/optimism-regnet/vnets"
+const TenderlyBaseURL = "https://api.tenderly.co/api/v1/account/anchorage/project/base-regnet/vnets"
+const TenderlyBobURL = "https://api.tenderly.co/api/v1/account/anchorage/project/bob-regnet/vnets"
+
+var tenderlyProjectsUrls = []string{TenderlyOptimismURL, TenderlyBaseURL, TenderlyBobURL}
+
 func main() {
 	client := &http.Client{}
-	var page int = 10
-	for page > 1 {
-		req, _ := http.NewRequest("GET", "https://api.tenderly.co/api/v1/account/anchorage/project/optimism-regnet/vnets", nil)
-		req.Header.Add("X-Access-Key", "EharCQ2AJxJoFgbIL3Yx3kcMNcJlSQTE")
-		q := req.URL.Query()
-		q.Add("perPage", "20")
-		req.URL.RawQuery = q.Encode()
-		fmt.Printf("getting page %d\n", page)
-		resp, _ := client.Do(req)
-		fmt.Println(resp.StatusCode)
-		body, _ := io.ReadAll(resp.Body)
-		vnets := []Vnet{}
-		err := json.Unmarshal(body, &vnets)
+
+	for _, tenderlyProjectUrl := range tenderlyProjectsUrls {
+		// Create GET request
+		req, err := http.NewRequest("GET", tenderlyProjectUrl, nil)
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("Error creating GET request:", err)
 			return
 		}
-		for _, v := range vnets {
-			uri := fmt.Sprintf("https://api.tenderly.co/api/v1/account/anchorage/project/optimism-regnet/vnets/%s", v.Id)
-			req, _ = http.NewRequest("DELETE", uri, nil)
-			req.Header.Add("X-Access-Key", "EharCQ2AJxJoFgbIL3Yx3kcMNcJlSQTE")
-			resp, _ := client.Do(req)
-			fmt.Println(resp.StatusCode)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
+
+		req.Header.Add("X-Access-Key", "EharCQ2AJxJoFgbIL3Yx3kcMNcJlSQTE")
+
+		// Add query parameters
+		q := req.URL.Query()
+		q.Add("perPage", "20")
+		q.Add("page", "2")
+		req.URL.RawQuery = q.Encode()
+
+		fmt.Println("Request URL:", req.URL.String())
+
+		// Execute GET request
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println("Error executing GET request:", err)
+			return
 		}
-		page--
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			fmt.Println("Error: GET request failed with status", resp.StatusCode)
+			return
+		}
+
+		// Read response body
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error reading response body:", err)
+			return
+		}
+
+		// Parse JSON response
+		var vnets []Vnet
+		if err := json.Unmarshal(body, &vnets); err != nil {
+			fmt.Println("Error decoding JSON:", err)
+			continue
+		}
+
+		// Loop through Vnets and send DELETE requests
+		for _, v := range vnets {
+			uri := fmt.Sprintf("%s/%s", tenderlyProjectUrl, v.Id)
+
+			req, err := http.NewRequest("DELETE", uri, nil)
+			if err != nil {
+				fmt.Println("Error creating DELETE request:", err)
+				continue
+			}
+
+			req.Header.Add("X-Access-Key", "EharCQ2AJxJoFgbIL3Yx3kcMNcJlSQTE")
+
+			// Execute DELETE request
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Println("Error executing DELETE request:", err)
+				continue
+			}
+			defer resp.Body.Close()
+
+			fmt.Printf("Deleted Vnet %s - Status: %d\n", v.Id, resp.StatusCode)
+		}
 	}
 }
